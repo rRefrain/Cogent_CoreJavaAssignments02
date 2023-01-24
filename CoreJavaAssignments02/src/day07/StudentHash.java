4492a8e45ef89260814c1f9aafb481b458f4c1cc
michaelmiranda <mimiranda300@gmail.com>
2023-01-24 04:43:24 -1000

    Added Question02 Specs

--- CoreJavaAssignments02/src/day07/StudentHash.java
+++ CoreJavaAssignments02/src/day07/StudentHash.java
@@ -1,8 +1,32 @@
 package day07;
 
+import java.util.HashMap;
+
 public class StudentHash {
 	
+	private final HashMap<Integer, Student> map = new HashMap<>();
+	
 	public void setName() {
+		
+	}
+	
+	public void printNames(){
+		
+	}
+	
+	public void getName(String key) {
+		
+	}
+	
+	public void printNamesKeySet() {
+		
+	}
+	
+	public void printSize() {
+		
+	}
+	
+	public void remove() {
 		
 	}
 }
